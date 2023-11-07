8f99eeb88119aaa4f367970092f854e1f1a7aac0
Alexey Popov <alexey.popov.webdev@outlook.com>
2023-11-07 17:33:44 +0300

    change models

--- web/models.py
+++ web/models.py
@@ -4,56 +4,71 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class CourseList(models.Model):
-    course_list_title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
     change_date = models.DateTimeField()
-    course_list_info = models.CharField(max_length=1024)
+    info = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
 class Course(models.Model):
-    course_title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
     subscribers = models.IntegerField()
-    course_info = models.CharField(max_length=1024)
-    course_img = models.ImageField(upload_to='courses/', null=True, blank=True)
-    course_list = models.ForeignKey(CourseList)
+    info = models.TextField()
+    img = models.ImageField(upload_to='courses/', null=True, blank=True)
+    list = models.ForeignKey(CourseList)
+
 
 class Lesson(models.Model):
-    lesson_title = models.CharField(max_length=256)
-    lesson_info = models.CharField(max_length=1024)
-    lesson_img = models.ImageField(upload_to='lessons/', null=True, blank=True)
+    title = models.CharField(max_length=256)
+    info = models.TextField()
+    img = models.ImageField(upload_to='lessons/', null=True, blank=True)
     complete_percent = models.IntegerField()
-    course = models.ForeignKey(Course)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
 
 class LessonTask(models.Model):
-    task_info = models.CharField(max_length=256)
-    is_completed = models.BooleanField(default=False)
-    lesson = models.ForeignKey(Lesson)
+    task = models.CharField(max_length=256)
+    is_complete = models.BooleanField(default=False)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+
+class LessonPart(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
 
 class LessonLink(models.Model):
     link = models.URLField() 
-    lesson = models.ForeignKey(Lesson)
+    part = models.ForeignKey(LessonPart, on_delete=models.CASCADE)
+
 
 class LessonVideo(models.Model):
     video_url = models.URLField()
-    lesson = models.ForeignKey(Lesson)
-    
+    part = models.ForeignKey(LessonPart, on_delete=models.CASCADE)
+
+
 class LessonArticle(models.Model):
-    article = models.CharField(max_length=4096)
-    lesson = models.ForeignKey(Lesson)
+    article = models.TextField()
+    part = models.ForeignKey(LessonPart, on_delete=models.CASCADE)
+
 
 class LessonTest(models.Model):
-    test_title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
     is_completed = models.BooleanField(default=False)
     complete_percent = models.IntegerField(max_length=100)
-    lesson = models.ForeignKey(Lesson)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class TestQuestion(models.Model):
     question = models.CharField(max_length=1024)
     is_complete = models.BooleanField(default=False)
     test = models.ForeignKey(LessonTest)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class TestAnswer(models.Model):
     answer = models.CharField(max_length=1024)
-    is_true = models.BooleanField(default=False)
+    is_correct = models.BooleanField(default=False)
     question = models.ForeignKey(TestQuestion)
 
 
